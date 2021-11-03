3a8b3f542717ce146532a78f6cd6b6b1bc972c1e
Thekote <dkglima@gmail.com>
2021-11-03 19:27:18 -0300

    Add category and product relations

--- backend/models/category.js
+++ backend/models/category.js
@@ -2,20 +2,19 @@
 const {
   Model
 } = require('sequelize');
+const product = require('./product');
 module.exports = (sequelize, DataTypes) => {
   class Category extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
+
     static associate(models) {
-      // define association here
+      Category.hasMany(models.Product, {
+          foreignKey: categoryId });
     }
   };
   Category.init({
     description: DataTypes.STRING,
-    cod: DataTypes.STRING
+    cod: DataTypes.STRING,
+    isActive: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'Category',
