e5b8e85c334ed6382f93b64f0ab34de261cc0da6
Mário Menezes <mario082@gmail.com>
2021-11-16 18:23:57 -0300

    App.js comments cleaning

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -33,12 +33,3 @@ const ContentContainer = styled.div`
 
 export default App
 
-
-
-// Rubik Link:
-
-// <link rel="preconnect" href="https://fonts.googleapis.com">
-// <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-// <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
-
-// font-family: 'Rubik', sans-serif;
