51caf123ddf41333f132a19ac69fd2b82332aee8
Guilherme Monteiro <dkglima@gmail.com>
2021-12-01 17:20:16 -0300

    feat: add formik to handle form

--- frontend/src/pages/AddCategory/index.js
+++ frontend/src/pages/AddCategory/index.js
@@ -1,77 +1,65 @@
-import React, { useState } from "react"
+import React from "react"
 import CategoryDataService from "../../client/category"
 import styled from "styled-components"
 import { toast } from "react-toastify"
+import { Formik, Field, Form } from "formik"
 
 const initialCategoryState = {
   description: "",
   cod: "",
   isActive: true,
 }
-
-const AddCategory = () => {
-  const [category, setCategory] = useState(initialCategoryState)
-
-  const handleInputChange = (event) => {
-    const { name, value } = event.target
-    setCategory({ ...category, [name]: value })
-  }
-
-  const createCategory = (event) => {
-    event.preventDefault()
-
-    CategoryDataService.create(category).then(() => {
+const createCategory = async (values, { resetForm }) => {
+  return CategoryDataService.create(values)
+    .then(() => {
       toast.success("Categoria criada com sucesso!!")
       resetForm()
     })
-  }
-
-  const resetForm = () => {
-    setCategory(initialCategoryState)
-  }
-
+    .catch((err) => {
+      toast.error("Erro ao cadastrar categoria")
+    })
+}
+const AddCategory = () => {
   return (
     <Container>
-      <FormContainer onSubmit={createCategory} onReset={resetForm}>
-        <div>
-          <h2>Cadastrar Categorias de Produtos</h2>
-          <div className="line"></div>
+      <Formik initialValues={initialCategoryState} onSubmit={createCategory}>
+        <FormContainer>
           <div>
+            <h2>Cadastrar Categorias de Produtos</h2>
+            <div className="line"></div>
             <div>
-              <label>Descrição</label> <br />
-              <input
-                type="text"
-                id="description"
-                required
-                value={category.description}
-                onChange={handleInputChange}
-                name="description"
-                placeholder="Ex: Utensílios"
-              />
+              <div>
+                <label>Descrição</label> <br />
+                <Field
+                  type="text"
+                  id="description"
+                  required
+                  name="description"
+                  placeholder="Ex: Utensílios"
+                />
+              </div>
+              <div>
+                <label>Código da Categoria</label> <br />
+                <Field
+                  type="text"
+                  id="cod"
+                  required
+                  name="cod"
+                  placeholder="Ex: UTN"
+                />
+              </div>
+              <br />
+              <button type="submit" className="btn btn-success">
+                Cadastrar Categoria
+              </button>
+              <br />
+              <button type="reset" className="btn btn-cancel">
+                Recomeçar
+              </button>
             </div>
-            <div>
-              <label>Código da Categoria</label> <br />
-              <input
-                type="text"
-                id="cod"
-                required
-                value={category.cod}
-                onChange={handleInputChange}
-                name="cod"
-                placeholder="Ex: UTN"
-              />
-            </div>
-            <br />{" "}
-            <button type="submit" className="btn btn-success">
-              Cadastrar Categoria
-            </button>
-            <br />{" "}
-            <button type="reset" className="btn btn-cancel">
-              Recomeçar
-            </button>
           </div>
-        </div>
-      </FormContainer>
+        </FormContainer>
+      </Formik>
     </Container>
   )
 }
@@ -80,7 +68,7 @@ const Container = styled.div`
   display: flex;
   justify-content: center;
 `
-const FormContainer = styled.form`
+const FormContainer = styled(Form)`
   display: flex;
   justify-content: center;
   width: 30vw;
@@ -103,6 +91,17 @@ const FormContainer = styled.form`
     border: 1px solid black;
     border-radius: 5px;
     padding-left: 10px;
+    &.has-error {
+      border: 1px solid red;
+      margin-bottom: 2px;
+    }
+  }
+
+  .error {
+    display: block;
+    font-size: 12px;
+    color: red;
+    margin: 0 0 5px 5px;
   }
 
   h2 {
