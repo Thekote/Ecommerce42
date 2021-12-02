d78ae660131fc34d570939ed37f89180bce81381
Mário Menezes <mario082@gmail.com>
2021-12-02 04:26:57 -0300

    SliderContent with styled components

--- frontend/src/components/BannerSlider/SliderContent.js
+++ frontend/src/components/BannerSlider/SliderContent.js
@@ -1,8 +1,9 @@
 import React from "react"
+import styled from "styled-components"
 
 function SliderContent({ activeIndex, sliderImage }) {
   return (
-    <section>
+    <Container>
       {sliderImage.map((slide, index) => (
         <div
           key={index}
@@ -13,8 +14,49 @@ function SliderContent({ activeIndex, sliderImage }) {
           <h3 className="slide-text">{slide.description}</h3>
         </div>
       ))}
-    </section>
+    </Container>
   )
 }
+
+const Container = styled.section`
+  .active {
+    display: inline-block;
+  }
+
+  .inactive {
+    display: none;
+  }
+
+  .slides {
+    height: var(--heights);
+    width: var(--widths);
+    position: relative;
+  }
+
+  .slide-image {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    object-fit: cover;
+  }
+
+  .slide-title,
+  .slide-text {
+    width: 100%;
+    height: 100%;
+    color: white;
+    font-size: 50px;
+    position: absolute;
+    text-align: center;
+    top: 40%;
+    z-index: 10;
+  }
+
+  .slide-text {
+    top: 65%;
+    font-size: 2rem;
+  }
+
+`
 
 export default SliderContent
