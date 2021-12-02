1009177be1bb8cc82318f4f40ada237cb0e3d0a2
Mário Menezes <mario082@gmail.com>
2021-12-02 03:53:25 -0300

    Arrows with styled-components

--- frontend/src/components/BannerSlider/Arrows.js
+++ frontend/src/components/BannerSlider/Arrows.js
@@ -1,4 +1,5 @@
 import React from "react"
+import styled from "styled-components"
 
 function Arrows({ prevSlide, nextSlide }) {
   return (
@@ -12,5 +13,35 @@ function Arrows({ prevSlide, nextSlide }) {
     </div>
   )
 }
+
+const Container = styled.div`
+
+  .prev,
+  .next {
+  cursor: pointer;
+  z-index: 100;
+  position: absolute;
+  top: 50%;
+  width: auto;
+  padding: 1rem;
+  margin-top: -3rem;
+  font-size: 40px;
+  font-weight: bold;
+  border-radius: 0px 5px 5px 0px;
+  }
+
+  .prev:hover,
+  .next:hover {
+  color: white;
+  background-color: rgba(0, 0, 0, 0.6);
+  transition: all 0.5s ease-in;
+  }
+
+  .next {
+  right: 0;
+  border-radius: 5px 0px 0px 5px;
+  }
+
+`
 
 export default Arrows
